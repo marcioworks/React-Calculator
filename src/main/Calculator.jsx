27fe2a83b55da2354bc0f02e88125b83b32e1064
Marcio Silva <marcioadsworks@gmail.com>
2020-01-19 20:35:16 -0300

    Calculator finished

--- src/main/Calculator.jsx
+++ src/main/Calculator.jsx
@@ -7,7 +7,7 @@ const initialState = {
     displayValue: '0',
     clearDisplay: false,
     operation: null,
-    value: [0, 0],
+    values: [0, 0],
     current: 0
 }
 
@@ -25,33 +25,56 @@ export default class Calculator extends Component {
     }
 
     clearMemory() {
-      this.setState({...initialState})
+        this.setState({ ...initialState })
     }
 
-    setOperation(op) {
-        console.log(op)
+    setOperation(operation) {
+        if (this.state.current === 0) {
+            this.setState({ operation, current: 1, clearDisplay: true })
+        } 
+        else {
+            const equals = operation === '='
+            const currentOperation = this.state.operation
+            const values =[...this.state.values]
+            try{
+                values[0] = eval(`${values[0]} ${currentOperation} ${values[1]}`)
+            }
+            catch(e){
+                values[0] = this.state.values[0]
+            }
+            values[1] = 0
+
+            this.setState({
+                displayValue: values[0],
+                operation:equals ? null :operation,
+                current: equals? 0: 1,
+                clearDisplay: !equals,
+                values
+            })
+        }
     }
 
     setDigit(n) {
-        if(n === '.' && this.state.displayValue.includes('.')){
+        if (n === '.' && this.state.displayValue.includes('.')) {
             return
         }
 
-        const clearDisplay = this.state.displayValue === '0' || this.state.clearDisplay
-        const currentValue = clearDisplay ?'': this.state.displayValue
-        const displayValue = currentValue + n
-        this.setState({displayValue,clearDisplay:false})
 
-        if(n !== '.'){
+        const clearDisplay = this.state.displayValue === '0' || this.state.clearDisplay
+        const currentValue = clearDisplay ? '' : this.state.displayValue
+        const displayValue = currentValue + n
+        this.setState({ displayValue, clearDisplay: false })
+
+        if (n !== '.') {
             const i = this.state.current
             const newValue = parseFloat(displayValue)
-            const values = {...this.state.values}
+            const values = [ ...this.state.values ]
             values[i] = newValue
-            this.setState({values})
+            this.setState({ values })
             console.log(values)
         }
 
-        
+
     }
 
     render() {
